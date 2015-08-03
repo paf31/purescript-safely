bb970fc92d64a14c254d549032bcb166fc80256b
Phil Freeman <paf31@cantab.net>
2015-08-03 16:08:38 -0700

    Simpler test

--- test/Main.purs
+++ test/Main.purs
@@ -12,9 +12,6 @@ import Control.Monad.Eff.Console
 replicateS :: forall m a. (MonadRec m) => Int -> m a -> m (List a)
 replicateS n m = safely \up -> replicateM n (up m)
 
-safe :: Eff _ (List Unit)
-safe = replicateS 100000 (log "Testing...")
-
 main = do
-  safe
+  replicateS 100000 (log "Testing...")
   log "If you can see this, everything worked."
