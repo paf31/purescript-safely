8f96066a74e34fd2aaa38201f8508f67f8605501
Phil Freeman <paf31@cantab.net>
2015-08-04 14:28:45 -0700

    Safer API

--- src/Control/Safely.purs
+++ src/Control/Safely.purs
@@ -1,7 +1,4 @@
-module Control.Safely 
-  ( MMorph()
-  , safely
-  ) where
+module Control.Safely where
 
 import Prelude
 
@@ -12,6 +9,12 @@ import Control.Monad.Rec.Class (MonadRec)
 -- | A monad morphism.
 type MMorph f g = forall a. f a -> g a
 
--- | Make a monadic action stack-safe.
-safely :: forall m a. (MonadRec m) => (forall t. (Monad t) => MMorph m t -> t a) -> m a
-safely f = runProcess (f lift)
+-- | A control operator is an operator which works on any `Monad`,
+-- | so that we can change the monad by providing monad morphisms
+-- | to wrap and unwrap monadic actions.
+class Operator o where
+  mapO :: forall m n. MMorph m n -> MMorph n m -> o m -> o n
+
+-- | Make a control operator stack-safe.
+safely :: forall o m a. (Operator o, MonadRec m) => (forall t. (Monad t) => o t) -> o m
+safely o = mapO runProcess lift o
--- test/Main.purs
+++ test/Main.purs
@@ -9,8 +9,16 @@ import Control.Monad.Rec.Class
 import Control.Monad.Eff
 import Control.Monad.Eff.Console
 
+newtype Replicator m = Replicator (forall a. Int -> m a -> m (List a))
+
+instance replicator :: Operator Replicator where
+  mapO to fro (Replicator r) = Replicator \n m -> to (r n (fro m))
+
+runReplicator :: forall m a. Replicator m -> Int -> m a -> m (List a)
+runReplicator (Replicator r) = r
+
 replicateS :: forall m a. (MonadRec m) => Int -> m a -> m (List a)
-replicateS n m = safely \up -> replicateM n (up m)
+replicateS = runReplicator (safely (Replicator replicateM))
 
 main = do
   replicateS 100000 (log "Testing...")
